f0aff5254c1b90bbae076ef6769a9f29e87029af
Ruan Lucas <rulucasilva13@gmail.com>
2025-01-04 19:19:56 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,13 @@ public class Program {
 		Seller newSeller = new Seller(null,"Ruan", "ruan@gmail.com", new Date(), 5000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		System.out.println("*** TEST 5: Seller UPDATE***");
+		seller = sellerDao.findById(1);
+		seller.setName("Joao");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
